70e19b51970a6b70b21eab0cce3ab6dbe664553f
Akhuemonkhan Olugbenga <gbengaloveeyesimages@gmail.com>
2019-06-13 17:39:24 -0400

    email validator

---
+++ main.js
@@ -0,0 +1,13 @@
+var emailAddress = "";
+    var text ="";
+
+
+ do { emailAddress = prompt("Enter a valid email address:");
+    } 
+while (emailAddress.indexOf("@") === -1) 
+    
+
+document.getElementById("output").innerHTML = text;
+
+
+
